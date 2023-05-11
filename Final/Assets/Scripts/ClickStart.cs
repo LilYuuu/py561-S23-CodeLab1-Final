4dd5aeb5759a57a7ff64ece9199a9e55964d8abb
Lily Yu <>
2023-05-11 14:46:08 -0400

    added starting scene

---
+++ Final/Assets/Scripts/ClickStart.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickStart : MonoBehaviour
+{
+    private void OnMouseUp()
+    {
+        
+        SceneManager.LoadScene(1);
+    }
+}
